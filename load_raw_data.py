9eb71e4bc371a973ae7e67fb9ae7db4823856873
bastien-winant <armand.winant@gmail.com>
2025-09-10 23:54:17 +0200

    create features staging model

--- load_raw_data.py
+++ load_raw_data.py
@@ -1,8 +1,9 @@
 import duckdb
 
 with duckdb.connect("./trail_trekker.db") as con:
-	con.sql("CREATE OR REPLACE TABLE subscriptions AS SELECT * FROM './data/subscriptions.csv'")
-	con.sql("CREATE OR REPLACE TABLE plan_features AS SELECT * FROM './data/plan_features.csv'")
-	con.sql("CREATE OR REPLACE TABLE customers AS SELECT * FROM './data/customers.csv'")
-	con.sql("CREATE OR REPLACE TABLE features AS SELECT * FROM './data/features.csv'")
-	con.sql("CREATE OR REPLACE TABLE plans AS SELECT * FROM './data/plans.csv'")
+	con.sql("CREATE OR REPLACE SCHEMA raw;")
+	con.sql("CREATE OR REPLACE TABLE raw.subscriptions AS SELECT * FROM './data/subscriptions.csv'")
+	con.sql("CREATE OR REPLACE TABLE raw.plan_features AS SELECT * FROM './data/plan_features.csv'")
+	con.sql("CREATE OR REPLACE TABLE raw.customers AS SELECT * FROM './data/customers.csv'")
+	con.sql("CREATE OR REPLACE TABLE raw.features AS SELECT * FROM './data/features.csv'")
+	con.sql("CREATE OR REPLACE TABLE raw.plans AS SELECT * FROM './data/plans.csv'")
