0d676d8f6bcd947a7198beae0d8e32e3304bdf7e
bastien-winant <armand.winant@gmail.com>
2025-09-10 21:56:31 +0200

    read from local files

--- load_raw_data.sql
+++ load_raw_data.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE TABLE plans AS SELECT * FROM 'https://raw.githubusercontent.com/schottma/trail_trekker/refs/heads/main/plans.csv';
-CREATE OR REPLACE TABLE features AS SELECT * FROM 'https://raw.githubusercontent.com/schottma/trail_trekker/refs/heads/main/features.csv';
-CREATE OR REPLACE TABLE customers AS SELECT * FROM 'https://raw.githubusercontent.com/schottma/trail_trekker/refs/heads/main/customers.csv';
-CREATE OR REPLACE TABLE plan_features AS SELECT * FROM 'https://raw.githubusercontent.com/schottma/trail_trekker/refs/heads/main/plan_features.csv';
-CREATE OR REPLACE TABLE subscriptions AS SELECT * FROM 'https://raw.githubusercontent.com/schottma/trail_trekker/refs/heads/main/subscriptions.csv';
+CREATE OR REPLACE TABLE plans AS SELECT * FROM './data/plans.csv';
+CREATE OR REPLACE TABLE features AS SELECT * FROM './data/features.csv';
+CREATE OR REPLACE TABLE customers AS SELECT * FROM './data/customers.csv';
+CREATE OR REPLACE TABLE plan_features AS SELECT * FROM './data/plan_features.csv';
+CREATE OR REPLACE TABLE subscriptions AS SELECT * FROM './data/subscriptions.csv';
