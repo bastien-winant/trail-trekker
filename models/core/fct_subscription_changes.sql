3964de3547b30688cd874b589bacabe6bb68dc51
bastien-winant <armand.winant@gmail.com>
2025-09-18 00:38:40 +0200

    add cancellations to subscription change fact model

--- models/core/fct_subscription_changes.sql
+++ models/core/fct_subscription_changes.sql
@@ -1,25 +1,27 @@
 MODEL (
 	name core.fct_subscription_changes,
   kind VIEW,
-  grain (subscription_id, change_date),
+  grain (customer_id, change_date),
   audits (
-    NOT_NULL(columns := (subscription_id, customer_id, change_date))
+    NOT_NULL(columns := (customer_id, change_date))
   )
 );
 
 WITH lagged_subscriptions AS (
 	SELECT
-		id,
 		customer_id,
 		plan_id,
 		start_date,
+		end_date,
+		status,
 		LAG(plan_id) OVER (PARTITION BY customer_id ORDER BY start_date) AS previous_plan_id,
-		LAG(end_date) OVER (PARTITION BY customer_id ORDER BY start_date) AS previous_plan_end_date
+		LAG(end_date) OVER (PARTITION BY customer_id ORDER BY start_date) AS previous_plan_end_date,
+		LEAD(plan_id) OVER (PARTITION BY customer_id ORDER BY start_date) AS next_plan_id,
+		LEAD(start_date) OVER (PARTITION BY customer_id ORDER BY start_date) AS next_plan_start_date
 	FROM stg.subscriptions
 ),
 subscription_changes AS (
 	SELECT
-		id AS subscription_id,
 		customer_id,
 		'change' AS change_type,
 		start_date AS change_date,
@@ -28,7 +30,21 @@ subscription_changes AS (
 	FROM lagged_subscriptions
 	WHERE previous_plan_id IS NOT NULL
 	AND start_date = previous_plan_end_date
+),
+subscription_cancellations AS (
+	SELECT
+		customer_id,
+		'cancellation' AS change_type,
+		end_date AS change_date,
+		plan_id,
+		previous_plan_id
+	FROM lagged_subscriptions
+	WHERE (next_plan_start_date IS NULL AND status = 'cancelled')
+	OR (next_plan_start_date IS NOT NULL AND end_date <> next_plan_start_date)
 )
 SELECT *
 FROM subscription_changes
-ORDER BY customer_id, change_date;
+UNION
+SELECT *
+FROM subscription_cancellations
+ORDER BY change_date;
