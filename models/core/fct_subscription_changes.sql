5c53dfaf2de12117bc782c4e4a1cd2a1a3fd346f
bastien-winant <armand.winant@gmail.com>
2025-09-19 00:29:17 +0200

    refactor subscription changes model

---
+++ models/core/fct_subscription_changes.sql
@@ -0,0 +1,36 @@
+MODEL (
+  name core.fct_subscription_changes,
+	kind VIEW,
+	grain (subscription_id, change_date),
+	audits (
+		not_null(columns := (subscription_id, customer_id, change_type, change_date))
+	)
+);
+
+WITH lagged_subscriptions AS (
+	SELECT
+		id AS subscription_id,
+		customer_id,
+		plan_id,
+		start_date,
+		end_date,
+		status,
+		LEAD(id) OVER (PARTITION BY customer_id ORDER BY start_date) AS next_subscription_id,
+		LEAD(plan_id) OVER (PARTITION BY customer_id ORDER BY start_date) AS next_plan_id,
+		LEAD(start_date) OVER (PARTITION BY customer_id ORDER BY start_date) AS next_plan_start_date
+	FROM stg.subscriptions
+),
+subscription_changes AS (
+	SELECT
+		subscription_id,
+		customer_id,
+		CASE
+			WHEN subscription_id = next_subscription_id THEN 'switch'
+			WHEN subscription_id <> next_subscription_id THEN 'cancellation'
+		END AS change_type,
+		end_date AS change_date
+	FROM lagged_subscriptions
+	WHERE status = 'cancelled'
+)
+SELECT * FROM subscription_changes
+ORDER BY subscription_id;
